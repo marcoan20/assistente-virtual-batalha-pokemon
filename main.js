647c9cd10b8d0734d5da51f616b9eb32669a73bd
Marco Antonio Gomes Pierozan <48890522+marcoan20@users.noreply.github.com>
2022-11-01 09:02:23 -0300

    First Commit

---
+++ main.js
@@ -0,0 +1,62 @@
+const msgerForm = get(".msger-inputarea");
+const msgerInput = get(".msger-input");
+const msgerChat = get(".msger-chat");
+
+// Icons made by Freepik from www.flaticon.com
+const BOT_IMG = "/464007.svg";
+const PERSON_IMG = "red.svg";
+const BOT_NAME = "BOT";
+const PERSON_NAME = "Red";
+
+window.onload = function () {
+  appendMessage(BOT_NAME, BOT_IMG, "left", "Como posso te ajudar treinador?");  
+};
+
+msgerForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const msgText = msgerInput.value;
+  if (!msgText) return;
+
+  appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
+  msgerInput.value = "";
+});
+
+function appendMessage(name, img, side, text) {
+  //   Simple solution for small apps
+  const msgHTML = `
+    <div class="msg ${side}-msg">
+      <div class="msg-img" style="background-image: url(${img})"></div>
+
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">${name}</div>
+          <div class="msg-info-time">${formatDate(new Date())}</div>
+        </div>
+
+        <div class="msg-text">${text}</div>
+      </div>
+    </div>
+  `;
+
+  msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+  msgerChat.scrollTop += 500;
+}
+
+// Utils
+function get(selector, root = document) {
+  return root.querySelector(selector);
+}
+
+function formatDate(date) {
+  const h = "0" + date.getHours();
+  const m = "0" + date.getMinutes();
+
+  return `${h.slice(-2)}:${m.slice(-2)}`;
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+export { };
